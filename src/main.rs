404e7439cbd6b549a4679d329243dc6009b8b1fc
Kieran Llarena <baymax709@gmail.com>
2025-08-26 17:59:09 -0400

    feat: print when current focused app changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use objc2::runtime::*;
+use objc2::*;
+use objc2_app_kit::*;
+use objc2_foundation::*;
+
+fn register_class() -> &'static AnyClass {
+    let mut builder = ClassBuilder::new(c"AppObserver", NSObject::class())
+        .expect("a class with name AppObserver likely already exists.");
+
+    unsafe extern "C" fn init(this: *mut AnyObject, _sel: Sel) -> *mut AnyObject {
+        let this: *mut AnyObject = msg_send![super(this, NSObject::class()), init];
+        this
+    }
+    unsafe {
+        builder.add_method(
+            sel!(init),
+            init as unsafe extern "C" fn(*mut AnyObject, Sel) -> *mut AnyObject,
+        );
+    }
+
+    unsafe extern "C" fn application_activated(
+        _this: *mut AnyObject,
+        _sel: Sel,
+        notification: *mut NSNotification,
+    ) {
+        unsafe {
+            let dereference_notif: &mut NSNotification = &mut *notification;
+            let app = dereference_notif.userInfo();
+            println!("Application activated: {:?}", app);
+        }
+    }
+    unsafe {
+        builder.add_method(
+            sel!(applicationActivated:),
+            application_activated as unsafe extern "C" fn(*mut AnyObject, Sel, *mut NSNotification),
+        );
+    }
+
+    builder.register()
+}
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        let app_observer_class = register_class();
+        let observer: *mut AnyObject = msg_send![app_observer_class, alloc];
+        let observer: *mut AnyObject = msg_send![observer, init];
+
+        let workspace = NSWorkspace::sharedWorkspace();
+        let notification_center = workspace.notificationCenter();
+
+        notification_center.addObserver_selector_name_object(
+            &*(observer as *const AnyObject),
+            sel!(applicationActivated:),
+            Some(NSWorkspaceDidActivateApplicationNotification),
+            None,
+        );
+
+        println!("Monitoring application activations. Press Ctrl+C to stop.");
+        let current_run_loop = NSRunLoop::currentRunLoop();
+        current_run_loop.run();
+    }
 }
